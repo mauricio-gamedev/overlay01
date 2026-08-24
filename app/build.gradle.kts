a31e609a56b358ce750bd6801a6e6ad0bee47faa
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-24 11:50:54 -0400

    chore: bump Overlay01 to v0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.mauriciogamedev.overlay01"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
@@ -24,4 +24,7 @@ android {
             )
         }
     }
+}
+
+dependencies {
 }
